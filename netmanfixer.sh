bf7a34f30e9fbd11685ca651a3b2340c1e8dadd7
0xberkay <o200706008@gmail.com>
2022-10-25 20:23:40 +0300

    Update netmanfixer.sh

--- netmanfixer.sh
+++ netmanfixer.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-#https://berkaykucuk.com.tr
-# network manager hatası && network maneger error
- echo "birazdan network yöneticini tamir edicem gelen şeylere evet de..."
- sleep 3
- clear
  sudo apt purge network-manager network-manager-gnome
  sudo rm -r /etc/network
  sudo rm -r /etc/NetworkManager 
  sudo apt install network-manager network-manager-gnome wireless-regdb wireless-tools 
- clear
- echo -e "\e[5mnetwork maneger fixed"
+
